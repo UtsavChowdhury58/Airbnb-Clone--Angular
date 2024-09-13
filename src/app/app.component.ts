8096b96d9b441525f655e4455e60373b704add38
UtsavChowdhury <Utsav.Chowdhury@ril.com>
2024-09-13 12:15:10 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {ButtonModule} from "primeng/button";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ToastModule,ButtonModule,FontAwesomeModule],
+  providers:[MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  faIconsLibrary: FaIconLibrary=Inject(FaIconLibrary);
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  private initFontAwesome() {
+    
+    this.faIconsLibrary.addIcons(...fontAwesomeIcons);
+  }
   title = 'airbnb-clone-front';
 }
---
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -0,0 +1,16 @@
+import { Component, InputSignal, input } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss'
+})
+export class AvatarComponent {
+
+  imageUrl =input<string>();
+
+  avatarSize=input<"avatar-sm" | "avatar-xl">();
+
+}
